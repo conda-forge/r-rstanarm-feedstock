31e45c7e2a07ec84316c15dbd9b6f29d35ba049d
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-24 01:08:01 -0600

    fix configure script

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,9 @@
 
 export TBB_INC=${PREFIX}/include
 cp ${RECIPE_DIR}/Makevars src
+if [[ ${build_platform} != ${target_plaform} ]]; then
+    sed -i 's/R_HOME/BUILD_PREFIX/' configure
+fi
 
 export DISABLE_AUTOBREW=1
 $R CMD INSTALL --build . ${R_ARGS}
